3d9678915776a0b5613d5a0b4abe4d54e5ad1c90
handrex-max <handrex2022@gmail.com>
2025-03-24 03:39:18 +0000

    navbar

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector('.menu-icon')
+const menuIcon = document.querySelector('.menu-icon i')
+const sideBar = document.querySelector('.sidebar')
+
+
+
+
+
+
+
+
+
+menuBtn.onclick = function(){
+    sideBar.classList.toggle('open')
+    const isOpen = sideBar.classList.contains('open')
+
+    if(isOpen){
+        menuIcon.classList = 'bx bx-x'
+    }else{
+        menuIcon.classList = 'bx bx-menu'
+    }
+}
